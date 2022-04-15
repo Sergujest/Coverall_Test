8a38d7b03363aa5e6156a9eac19109ced7548810
sergujest <y.kasimtemel@gmail.com>
2022-04-15 17:49:28 +0300

    test change

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,7 +2,7 @@ package example
 
 object Hello {
   def greet(number: Int): String = {
-    println("test")
+    println("test change")
     if ( number < 18) "No beer" else "Beer for you"
   }
   def unCoveredMethod(number: Int): String =
